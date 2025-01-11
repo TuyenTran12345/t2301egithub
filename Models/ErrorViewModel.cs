dfee9b536d24d8adec288ba75edb5047b246e81e
tuyentran <trandinhtuyen2018@gmail.com>
2025-01-11 11:51:19 +0700

    asasd

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppGIT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# t2301egithub
-test
